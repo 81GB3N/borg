4c2f672d3f74ba2cf152dea2a7f4e14ef1f1171c
81GB3N <jonas.balukonis@gmail.com>
2023-12-08 10:18:11 +0200

    list

--- public/html/index.js
+++ public/html/index.js
@@ -2,9 +2,19 @@ const display = async () =>{
     const api = await fetch("http://borg.licejus.lt:1337/api/index");
     const index = await api.json();
     for(let names in index.data.attributes){
-        if(document.querySelector(`.${names}`)){
-            console.log(index.data.attributes[names])
+        if(document.querySelector(`.${names}`) && document.querySelector(`.${names}`).tagName !== 'UL'){
+            // console.log(index.data.attributes[names])
             document.querySelector(`.${names}`).innerHTML = index.data.attributes[names];
+        }
+        else if(document.querySelector(`.${names}`) && document.querySelector(`.${names}`).tagName == 'UL'){
+            let items = index.data.attributes[names].split("\n");
+            // console.log(items);
+            let list = document.querySelector(`.${names}`);
+            list.innerHTML=``;
+            for(let item in items){
+                list.innerHTML+=`<li style="color: black">${items[item]}</li>`;
+            }
+            // console.log(list);
         }
     }
     // console.log(index.data.attributes);
