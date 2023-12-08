e9e5f0e7cee834b95fa1ffd8bf07d8148939221a
81GB3N <jonas.balukonis@gmail.com>
2023-12-08 10:35:26 +0200

    lists-edited

--- public/html/index.js
+++ public/html/index.js
@@ -2,17 +2,17 @@ const display = async () =>{
     const api = await fetch("http://borg.licejus.lt:1337/api/index");
     const index = await api.json();
     for(let names in index.data.attributes){
-        if(document.querySelector(`.${names}`) && document.querySelector(`.${names}`).tagName !== 'UL'){
+        let selected = document.querySelector(`.${names}`);
+        if(selected &&  selected.tagName.toLowerCase() != 'ul'){
             // console.log(index.data.attributes[names])
             document.querySelector(`.${names}`).innerHTML = index.data.attributes[names];
         }
-        else if(document.querySelector(`.${names}`) && document.querySelector(`.${names}`).tagName == 'UL'){
+        else if(selected && selected.tagName.toLowerCase() == 'ul'){
             let items = index.data.attributes[names].split("\n");
             // console.log(items);
-            let list = document.querySelector(`.${names}`);
-            list.innerHTML=``;
+            selected.innerHTML=``;
             for(let item in items){
-                list.innerHTML+=`<li style="color: black">${items[item]}</li>`;
+                selected.innerHTML+=`<li style="color: black">${items[item]}</li>`;
             }
             // console.log(list);
         }
